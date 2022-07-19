0cbd0e504715c3fa8879d9c90d4e24190d1af12e
Olutola Paul <tobaforjesus@gmail.com>
2022-07-19 11:08:57 +0100

    More Sorting Algorithm

---
+++ Sorting/mergeSort.js
@@ -0,0 +1,37 @@
+/*
+ * Author: Olutola Oloruntobi Paul
+ * Date: 7/19/2022
+ * Function for implementing merge sort aldorithm
+ */
+
+const merge = (arr1, arr2) => {
+  let newArr = [];
+  let i = 0;
+  let j = 0;
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      newArr.push(arr1[i]);
+      i++;
+    } else {
+      newArr.push(arr2[j]);
+      j++;
+    }
+  }
+  while (i < arr1.length) {
+    newArr.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    newArr.push(arr2[j]);
+    j++;
+  }
+  return newArr;
+};
+
+const mergeSort = (arr) => {
+  if (arr.length === 1) return arr;
+  let mid = Math.floor(arr.length / 2);
+  let leftArr = arr.slice(0, mid);
+  let rightArr = arr.slice(mid);
+  return merge(mergeSort(rightArr), mergeSort(leftArr));
+};
